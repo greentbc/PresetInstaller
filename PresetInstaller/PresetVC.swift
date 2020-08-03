cdec5ca522eee9d67412d4637fb68d289ce7dee0
Tyler Stiffler <tyler@greentbc.com>
2020-08-03 14:27:24 -0400

    First Commit

---
+++ PresetInstaller/PresetVC.swift
@@ -0,0 +1,18 @@
+//
+//  PresetVC.swift
+//  PresetInstaller
+//
+//  Created by Tyler Stiffler on 11/7/19.
+//  Copyright © 2019 Tyler Stiffler. All rights reserved.
+//
+
+import Cocoa
+
+class PresetVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
